2973f8f8ffd5e66ed4e87b96e48a2d9f941ffbde
TonyBaldi <baldissera.antony@escola.pr.gov.br>
2025-08-11 07:46:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function mostraPalavraChave (){
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    for(let i in palavras){
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencia(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
 
@@ -19,7 +25,6 @@ function processaTexto(texto){
         return frequencias[p2] - frequencias[1];
     }
 
-    console.log(ordenadas);
     return ordenadas.slice();
 }
 
@@ -32,6 +37,139 @@ function contaFrequencia(palavras){
                 frequencias[i]++;
             }
         }
+    }  
+}
+
+function tiraPalavrasRuins(palavras){
+    const PALAVRAS_RUINS = new Set([
+    "que",
+    "para",
+    "com",
+    "não",
+    "uma",
+    "por",
+    "mais",
+    "dos",
+    "como",
+    "mas",
+    "foi",
+    "ele",
+    "das",
+    "tem",
+    "seu",
+    "sua",
+    "ser",
+    "quando",
+    "muito",
+    "está",
+    "também",
+    "pelo",
+    "pela",
+    "até",
+    "isso",
+    "ela",
+    "entre",
+    "era",
+    "depois",
+    "sem",
+    "mesmo",
+    "aos",
+    "ter",
+    "seus",
+    "quem",
+    "nas",
+    "esse",
+    "eles",
+    "estão",
+    "você",
+    "tinha",
+    "foram",
+    "essa",
+    "num",
+    "nem",
+    "suas",
+    "meu",
+    "minha",
+    "têm",
+    "numa",
+    "pelos",
+    "elas",
+    "havia",
+    "seja",
+    "qual",
+    "será",
+    "nós",
+    "tenho",
+    "lhe",
+    "deles",
+    "essas",
+    "esses",
+    "pelas",
+    "este",
+    "fosse",
+    "dele",
+    "vocês",
+    "vos",
+    "lhes",
+    "meus",
+    "minhas",
+    "teu",
+    "tua",
+    "teus",
+    "tuas",
+    "nosso",
+    "nossa",
+    "nossos",
+    "nossas",
+    "dela",
+    "delas",
+    "esta",
+    "estes",
+    "estas",
+    "aquele",
+    "aquela",
+    "aqueles",
+    "aquelas",
+    "isto",
+    "aquilo",
+    "estou",
+    "estamos",
+    "estive",
+    "esteve",
+    "estivemos",
+    "estiveram",
+    "estava",
+    "estávamos",
+    "estavam",
+    "e",
+    "ou",
+    "onde",
+    "aquilo",
+    "embora",
+    "apesar",
+    "porque",
+    "enquanto",
+    "contudo",
+    "entretanto",
+    "portanto",
+    "além",
+    "antes",
+    "todavia",
+    "inclusive",
+    "diante",
+    "sobre",
+    "quanto",
+    "apenas",
+    "desde",
+    "uma vez",
+    "segundo",
+    "junto"
+]);
+    const palavrasBoas = [];
+    for(let palavra of palavras){
+        if(!PALAVRAS_RUINS.has(palavra) && palavra.length > 2){
+            palavrasBoas.push(palavra);
+        }
     }
-    console.log(frequencias);   
+    return palavrasBoas;
 }
